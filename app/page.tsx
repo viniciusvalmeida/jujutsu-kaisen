9aa7f4058df870929d724226288c5bae7ee2bf64
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-10-29 17:23:10 -0300

    feat: Header and background

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,24 @@
+import Image from "next/image";
+
+const getAnimeData = async () => {
+    const res = fetch("");
+};
+
 export default function Home() {
-  return (
-   <h1>Home page</h1>
-  )
+    return (
+        <>
+            <header className="text-white text-center flex justify-center items-center">
+                <Image
+                    src="/logo.png"
+                    alt="Jujutsu Kaisen Logo"
+                    width={400}
+                    height={340}
+                />
+            </header>
+
+            <main>
+              
+            </main>
+        </>
+    );
 }
