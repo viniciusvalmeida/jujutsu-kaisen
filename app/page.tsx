1a6bd10a459937612e4659b8ba8016a396d75ed4
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-10-29 21:48:24 -0300

    dev: Type of fetch response

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,55 @@
 import Image from "next/image";
 
 type Character = {
-  character: {
-    mal_id: number
-    name: string
-  }
-}
+	character: {
+		mal_id: number;
+		name: string;
+	};
+};
 
 type FethResponse = {
-  data: Character[]
-}
+	data: Character[];
+};
 
-const getAnimeData = async (): Promise<FethResponse> => {
-  const res = await fetch("https://api.jikan.moe/v4/anime/40748/characters")
-    
-  return res.json()
+const getAnimeData = async (): Promise<FethResponse | void> => {
+	try {
+		const res = await fetch(
+			"https://api.jikan.moe/v4/anime/40748/characters"
+		);
+
+		return res.json();
+	} catch (err) {
+		console.error(err);
+	}
 };
 
 export default async function Home() {
-    const { data } = await getAnimeData()
-    
-    return (
-        <>
-            <header className="text-white text-center flex justify-center items-center">
-                <Image
-                    src="/logo.png"
-                    alt="Jujutsu Kaisen Logo"
-                    width={400}
-                    height={340}
-                />
-            </header>
+	const { data } = (await getAnimeData()) as FethResponse;
 
-            <main className="grid grid-cols-2">
-              <div className="bg-red-400/50">
-                <ul>
-                {data.map((char: Character) => (
-                  <li key={char.character.mal_id}>{char.character.name}</li>
-                ))}
-                </ul>
-              </div>
-              
-              <div className="bg-emerald-400/50">
-                2
-              </div>
-            </main>
-        </>
-    );
+	return (
+		<>
+			<header className="text-white text-center flex justify-center items-center">
+				<Image
+					src="/logo.png"
+					alt="Jujutsu Kaisen Logo"
+					width={400}
+					height={340}
+				/>
+			</header>
+
+			<main className="grid grid-cols-2">
+				<div className="bg-red-400/50">
+					<ul>
+						{data.map((char) => (
+							<li key={char.character.mal_id}>
+								{char.character.name}
+							</li>
+						))}
+					</ul>
+				</div>
+
+				<div className="bg-emerald-400/50">2</div>
+			</main>
+		</>
+	);
 }
