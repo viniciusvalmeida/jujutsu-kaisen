ba2e27d8257397043b60a7278d26cc62ab1cb229
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-11-03 17:39:15 -0300

    dev: responsive layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { log } from "console";
+import CharacterInfo from "@/components/CharacterInfo";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -25,19 +25,6 @@ type FethResponse = {
 	data: Character[];
 };
 
-type CharacterFull = {
-	data: {
-		mal_id: number;
-		name: string;
-		about: string;
-		images: {
-			webp: {
-				image_url: string;
-			};
-		};
-	};
-};
-
 const getAnimeData = async (): Promise<FethResponse | void> => {
 	try {
 		const res = await fetch(
@@ -52,27 +39,9 @@ const getAnimeData = async (): Promise<FethResponse | void> => {
 	}
 };
 
-const getCharacterInfo = async (
-	mal_id: number
-): Promise<CharacterFull | void> => {
-	try {
-		const res = await fetch(
-			`https://api.jikan.moe/v4/characters/${mal_id}/full`
-		);
-
-		if (!res.ok) return;
-
-		return res.json();
-	} catch (err) {
-		console.error(err);
-	}
-};
-
 export default async function Home({ searchParams }: HomeProps) {
 	const { data } = (await getAnimeData()) as FethResponse;
-	const character = (await getCharacterInfo(
-		+searchParams.mal_id || 164471
-	)) as CharacterFull;
+	const characterId = +searchParams.mal_id || 164471;
 
 	return (
 		<>
@@ -85,9 +54,9 @@ export default async function Home({ searchParams }: HomeProps) {
 				/>
 			</header>
 
-			<main className="grid grid-cols-2 gap-4">
-				<div className="bg-slate-400/60 rounded-xl p-4 shadow-2xl shadow-slate-500">
-					<ul className="grid grid-cols-3 gap-4">
+			<main className="grid grid-cols-2 gap-4 px-4 xl:px-0">
+				<div className="bg-slate-400/60 rounded-xl p-4 shadow-2xl shadow-slate-500 backdrop-blur-sm">
+					<ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 						{data.splice(0, 27).map((char) => (
 							<li
 								key={char.character.mal_id}
@@ -98,6 +67,7 @@ export default async function Home({ searchParams }: HomeProps) {
 									className="flex items-center gap-3"
 								>
 									<Image
+										className="rounded-xl"
 										src={
 											char.character.images.webp.image_url
 										}
@@ -112,20 +82,7 @@ export default async function Home({ searchParams }: HomeProps) {
 					</ul>
 				</div>
 
-				<div className="bg-slate-400/60 rounded-xl p-4 flex flex-col items-center space-y-4 shadow-2xl shadow-slate-500">
-					<Image
-						className="rounded-lg"
-						src={character.data.images.webp.image_url}
-						alt="Character Image"
-						width={250}
-						height={420}
-					/>
-
-					<h2 className="text-2xl font-bold">
-						{character.data.name}
-					</h2>
-					<p>{character.data.about}</p>
-				</div>
+				<CharacterInfo mal_id={characterId} />
 			</main>
 		</>
 	);
