28d4137af06114b8370e75a0e9bb91bf8dc3efb3
viniciusvalmeida <81371812+viniciusvalmeida@users.noreply.github.com>
2023-11-03 17:38:58 -0300

    feat: Refactory character info

--- components/CharacterInfo.tsx
+++ components/CharacterInfo.tsx
@@ -1,3 +1,48 @@
-export default function CharacterInfo() {
-	return <h1>teste</h1>;
+import Image from "next/image";
+
+type CharacterFull = {
+	data: {
+		mal_id: number;
+		name: string;
+		about: string;
+		images: {
+			webp: {
+				image_url: string;
+			};
+		};
+	};
+};
+
+const getCharacterInfo = async (
+	mal_id: number
+): Promise<CharacterFull | void> => {
+	try {
+		const res = await fetch(
+			`https://api.jikan.moe/v4/characters/${mal_id}/full`
+		);
+
+		if (!res.ok) return;
+
+		return res.json();
+	} catch (err) {
+		console.error(err);
+	}
+};
+
+export default async function CharacterInfo({ mal_id }: { mal_id: number }) {
+	const character = (await getCharacterInfo(mal_id)) as CharacterFull;
+	return (
+		<div className="bg-slate-400/60 rounded-xl p-4 flex flex-col items-center space-y-4 shadow-2xl shadow-slate-500 backdrop-blur-sm">
+			<Image
+				className="rounded-lg w-24 md:w-48 lg:w-72"
+				src={character.data.images.webp.image_url}
+				alt="Character Image"
+				width={250}
+				height={420}
+			/>
+
+			<h2 className="text-2xl font-bold">{character.data.name}</h2>
+			<p>{character.data.about}</p>
+		</div>
+	);
 }
